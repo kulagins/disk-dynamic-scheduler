92d302c584ca12d107e82dd6076c47e89800926b
Ruben Laso <ruben.laso.rodriguez@univie.ac.at>
2025-07-02 12:01:07 +0200

    Change experiments scripts to match options

--- script_experiment.sh
+++ script_experiment.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 pwd
 
-./fonda_scheduler 1000000 100 1 0.001 chipseq_200 3793245764 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 chipseq_2000 3793245764 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 chipseq_8000 3793245764 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 chipseq_15000 3793245764 1 no ../ machines.csv 
+BINARY="./build/fonda_scheduler"
+COMMON_ARGS="-m 1000000 -s 100 -r 1 -o 0.001 -a heft-bl -f input/machines.csv -d 2 -S"
 
-./fonda_scheduler 1000000 100 1 0.001 atacseq_200 2223941232 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 atacseq_2000 2223941232 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 atacseq_8000 2223941232 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 atacseq_15000 2223941232 1 no ../ machines.csv 
+# Run experiments with different workloads
+WORKLOADS=(
+    "atacseq_200 2223941232"
+    "atacseq_2000 2223941232"
+    "atacseq_8000 2223941232"
+    "atacseq_15000 2223941232"
+    "bacass_200 3793245764"
+    "bacass_2000 3793245764"
+    "bacass_8000 3793245764"
+    "bacass_15000 3793245764"
+)
 
-./fonda_scheduler 1000000 100 1 0.001 eager_200 25705994498 1 no ../ machines.csv
-./fonda_scheduler 1000000 100 1 0.001 eager_2000 25705994498 1 no ../ machines.csv
-./fonda_scheduler 1000000 100 1 0.001 eager_8000 25705994498 1 no ../ machines.csv
-./fonda_scheduler 1000000 100 1 0.001 eager_15000 25705994498 1 no ../ machines.csv
-
-./fonda_scheduler 1000000 100 1 0.001 methylseq_200 17027257018 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 methylseq_2000 17027257018 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 methylseq_8000 17027257018 1 no ../ machines.csv 
-./fonda_scheduler 1000000 100 1 0.001 methylseq_15000 17027257018 1 no ../ machines.csv 
+for workload in "${WORKLOADS[@]}"; do
+    IFS=' ' read -r -a params <<< "$workload"
+    COMMAND="$BINARY ${COMMON_ARGS} -w ${params[0]} -i ${params[1]}"
+    echo "Running command: $COMMAND"
+    $COMMAND
+done
